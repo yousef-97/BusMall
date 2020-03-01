4106ddd932d160d04217618c6f0b8f192b4ae16c
yousef-97 <alshenyousef@gmail.com>
2020-03-02 00:34:42 +0200

    final things

--- js/app.js
+++ js/app.js
@@ -4,23 +4,27 @@
 
 
 
-var itemName = ['bag','banana','bathroom','boots','breakfast','bubblegum','chair',
-'cthulhu','dog-duck','dragon','pen','pet-sweep','scissors','shark','sweep','tauntaun',
-'unicorn','usb','water-can','wine-glass'];
+var itemName = ['bag.jpg','banana.jpg','bathroom.jpg','boots.jpg','breakfast.jpg','bubblegum.jpg','chair.jpg',
+'cthulhu.jpg','dog-duck.jpg','dragon.jpg','pen.jpg','pet-sweep.jpg','scissors.jpg','shark.jpg','sweep.png','tauntaun.jpg',
+'unicorn.jpg','usb.gif','water-can.jpg','wine-glass.jpg'];
 
 var data = [];
+var totalClicks = 0;
+
 
 var theContener = document.getElementById('theImgs');
 var leftImg = document.getElementById('leftImg');
 var centerImg = document.getElementById('centerImg');
 var rightImg = document.getElementById('rightImg');
 
-
+var theName = [];
 
 //the Objects
 function Item(name){
-    this.name = name;
-    this.filePath = `imgs/assets/${this.name}.jpg`;
+    this.name = name.substr(0,name.length-4);
+    theName.push(this.name);
+    // console.log(this.name);
+    this.filePath = `imgs/assets/${name}`;
     data.push(this); //to put each object in arr so each index has aname and file path
     // console.log(this.filepath);
 }
@@ -30,11 +34,12 @@ for(var i = 0 ; i < itemName.length;i++){
 // console.log(data); i use it to check that (whate if i put data.push(this before all properites))
 var shownArr=[];
 function theRandomImg(){
+    
     while(true){
         var f = getRandomNum(0, itemName.length-1);
         var s = getRandomNum(0, itemName.length-1);
         var t = getRandomNum(0, itemName.length-1);
-        if(f!== s && s!==t){
+        if(f!== s && s!==t && f !==t){
             var randomLeft = data[f];
             var randomCenter = data[s];
             var randomRight = data[t];
@@ -49,36 +54,86 @@ function theRandomImg(){
             break;
         }
     }
-    shownArr.push(f,s,t);//for counting the how many the iten appear
+    shownArr.push(randomLeft.name,randomCenter.name,randomRight.name);//for counting the how many the iten appear
     console.log(shownArr);
 }
 theRandomImg();
 
 var pickedArr = [];
 // The Event
+var c = 0;
 theContener.addEventListener('click',function(event){
+    totalClicks++;
     switch (true) {
         case event.target.id === 'leftImg':
+            pickedArr.push(event.target.alt);
             theRandomImg();
             break;
         
         case event.target.id === 'centerImg':
+            pickedArr.push(event.target.alt);
             theRandomImg();
             break;
          
         case  event.target.id === 'rightImg':
+            pickedArr.push(event.target.alt);
             theRandomImg();
             break;       
-        
-        default:
-            break;
     }
-    if(event.target.id === 'leftImg' || event.target.id === 'centerImg' || event.target.id === 'rightImg'){
-        theRandomImg();
-    }
+    console.log(totalClicks);
+    
+    if(totalClicks === 25){
+        //remove event listener
+        leftImg.remove();
+        centerImg.remove();
+        rightImg.remove();
+        // console.log(shownArr);
+        // console.log('finished');
+        shownArr.sort();
+        pickedArr.sort();
+        shownArr.splice(shownArr.length-3,3);
+        // console.log(pickedArr);
+        // console.log(shownArr);
+        // for(var n = 0;n<12;n++){
+            document.write(`this item is appear ${iterations(shownArr)} and picked ${iterations(pickedArr)} times`)
+        // }
+      }
+    // console.log(pickedArr);
+    
+    // console.log(event.target);
+    
+    // if(event.target.id === 'leftImg' || event.target.id === 'centerImg' || event.target.id === 'rightImg'){
+    //     theRandomImg();
+    // }
 })
+var theFinalArr = [];
+function iterations(arr){
+    
+    for (var i = 0;i<itemName.length-1;i++){
+        var c =0;
+        while (arr.includes(theName[i])){
+            if (arr.length === 0){break;}
+            c++;
+            // console.log(c);
+            
+            var index = arr.indexOf(theName[i]);
+            if (index > -1) {
+                arr.splice(index, 1);
+                // console.log(arr);
+                
+            }
 
+        }
+        theFinalArr.push(c);
+        console.log(theFinalArr);
+        if (arr.length === 0){break;}
+        
+    }
+    return theFinalArr;
+}
 
+iterations(shownArr);
+iterations(pickedArr);
 
 
 
@@ -90,3 +145,4 @@ function getRandomNum(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
 }
+
